98407a446e6b012b7e89248d931a88a03cf05160
フィルム <waranthorn_c@outlook.com>
2024-03-28 10:01:02 +0700

    Initial commit

---
+++ VacQ/server.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const connectDB = require('./config/db');
+const cookieParser = require('cookie-parser');
+
+// Load env vars
+dotenv.config({path:'./config/config.env'});
+
+// Connect to database
+connectDB();
+
+// Route files
+const hospitals = require('./routes/hospitals');
+const auth = require('./routes/auth');
+const appointments = require('./routes/appointments');
+
+const app=express();
+
+// Cookie parser
+app.use(cookieParser());
+
+// add body parser
+app.use(express.json());
+app.use('/api/v1/hospitals', hospitals);
+app.use('/api/v1/auth', auth);
+app.use('/api/v1/appointments', appointments);
+
+
+
+const PORT = process.env.PORT || 5001;
+const server = app.listen(PORT, console.log('Server running in ', process.env.NODE_ENV, 'mode on port ', PORT));
+
+// Handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise)=> {
+    console.log(`Error: ${err.message}`);
+    // Close server & exit process
+    server.close(()=>process.exit(1));
+});
