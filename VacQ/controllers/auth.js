de0fcbf47cfecc33d689b9a7725057ccf0a17014
フィルム <waranthorn_c@outlook.com>
2024-04-04 15:28:04 +0700

    Chapter 12 & 13

--- VacQ/controllers/auth.js
+++ VacQ/controllers/auth.js
@@ -29,34 +29,47 @@ exports.register = async (req, res, next) => {
 //@route POST /api/v1/auth/login
 //@access Public
 exports.login = async (req, res, next) => {
-  const { email, password } = req.body;
+  try {
+    const { email, password } = req.body;
 
-  // Validate email & password
-  if (!email || !password) {
+    // Validate email & password
+    if (!email || !password) {
+      return res
+        .status(400)
+        .json({ success: false, msg: "Please provide an email and password" });
+    }
+
+    // Check for user
+    const user = await User.findOne({ email }).select("+password");
+
+    if (!user) {
+      return res
+        .status(400)
+        .json({ success: false, msg: "Invalid credentials" });
+    }
+
+    // Check if password matches
+    const isMatch = await user.matchPassword(password);
+
+    if (!isMatch) {
+      return res
+        .status(401)
+        .json({ success: false, msg: "Invalid credetials" });
+    }
+
+    // Create token
+    // const token=user.getSignedJwtToken();
+
+    // res.status(200).json({success: true, token});
+    sendTokenResponse(user, 200, res);
+  } catch (error) {
     return res
-      .status(400)
-      .json({ success: false, msg: "Please provide an email and password" });
+      .status(401)
+      .json({
+        success: false,
+        msg: "Cannot convert email or password to String",
+      });
   }
-
-  // Check for user
-  const user = await User.findOne({ email }).select("+password");
-
-  if (!user) {
-    return res.status(400).json({ success: false, msg: "Invalid credentials" });
-  }
-
-  // Check if password matches
-  const isMatch = await user.matchPassword(password);
-
-  if (!isMatch) {
-    return res.status(401).json({ success: false, msg: "Invalid credetials" });
-  }
-
-  // Create token
-  // const token=user.getSignedJwtToken();
-
-  // res.status(200).json({success: true, token});
-  sendTokenResponse(user, 200, res);
 };
 
 // Get token from mode, create cookie and send response
@@ -88,8 +101,21 @@ const sendTokenResponse = (user, statusCode, res) => {
 //@desc     Get current Logged in user
 //@route    POST /api/v1/auth/me
 //@access   Private
-
 exports.getMe = async (req, res, next) => {
   const user = await User.findById(req.user.id);
   res.status(200).json({ success: true, data: user });
+};
+
+//@desc     Get current Logged in user
+//@route    POST /api/v1/auth/me
+//@access   Private
+exports.logout = async (req, res, next) => {
+  res.cookie("token", "none", {
+    expires: new Date(Date.now() + 10 * 1000),
+    httpOnly: true,
+  });
+  res.status(200).json({
+    success: true,
+    data: {},
+  });
 };
